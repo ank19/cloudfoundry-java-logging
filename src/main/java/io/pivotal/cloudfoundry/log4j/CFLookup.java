d0b20e12dce8fd4638e6d33d983bb9b8f6e570e8
David Malone <dmalone@pivotal.io>
2015-04-06 13:25:09 -0400

    added a little bit of documentation to CFLookup.java, and added a more complete example pattern layout in log4j2.xml

--- src/main/java/io/pivotal/cloudfoundry/log4j/CFLookup.java
+++ src/main/java/io/pivotal/cloudfoundry/log4j/CFLookup.java
@@ -4,6 +4,13 @@ import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.config.plugins.Plugin;
 import org.apache.logging.log4j.core.lookup.StrLookup;
 
+/**
+ * A log4j v2 plugin that will lookup values from Cloud Foundry's VCAP_APPLICATION 
+ * environment variable so that those values can be used as custom placeholders in 
+ * your log4j pattern layouts. 
+ * @author dmalone <dmalone@pivotal.io>
+ *
+ */
 @Plugin(name = "cf", category = "Lookup")
 public class CFLookup implements StrLookup{
 
